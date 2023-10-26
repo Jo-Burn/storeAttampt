451c057fb467b18b35071745b058f9baa72827c3
Jo-Burn <106935232+Jo-Burn@users.noreply.github.com>
2023-10-26 10:44:51 -0400

    Add files via upload

---
+++ store.js
@@ -0,0 +1,11 @@
+let iN = document.getElementById('num');
+const sub = document.getElementById('sub').addEventListener('click', out) || localStorage.getItem('log');
+let output = document.getElementById('output');
+sub
+window.onload = out();
+function out() {
+console.log('hello world')
+iN
+output.innerHTML = iN.value;
+localStorage.setItem('log', iN.value)
+}
